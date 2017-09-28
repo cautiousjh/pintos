688e6ae48c2bb84f9df0e1eb4b6957e5ff93cc11
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-09-29 02:08:32 +0900

    sdf

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -20,6 +20,7 @@ void sema_self_test (void);
 /* Lock. */
 struct lock 
   {
+    int donate_priority;
     struct list_elem lockElem;
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
