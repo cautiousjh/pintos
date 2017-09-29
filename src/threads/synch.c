56cc838c5dbe8a26d2057040a05f3b1f630361bb
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-09-29 12:17:40 +0900

    afaf

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -117,15 +117,18 @@ sema_up (struct semaphore *sema)
     list_sort (&sema->waiters, priority_aux_func, NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
-
+/*
     intr_set_level (old_level);
     sema->value++;
     thread_yield();
   }
   else{
     intr_set_level (old_level);
-    sema->value++;
+    sema->value++;*/
   }
+  intr_set_level (old_level);
+    sema->value++;
+    thread_yield();
 }
 
 static void sema_test_helper (void *sema_);
