1618b1fe32c1f0383474890044fa28f01e0bcc78
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-09-29 13:48:37 +0900

    afaf

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -220,11 +220,11 @@ timer_interrupt (struct intr_frame *args UNUSED)
 
   if(thread_mlfqs){
     //per second
-    if(timer_ticks()/TIMER_FREQ)
+    if(! (timer_ticks()%TIMER_FREQ))
       mlfqs_set_load_avg();
-    mlfqs_set_recent_cpu(ticks/TIMER_FREQ);
+    mlfqs_set_recent_cpu(!(ticks%TIMER_FREQ));
     //every fourth tick
-    if(ticks/4)
+    if(!(ticks%4))
       mlfqs_update_priority();
   }
 }
