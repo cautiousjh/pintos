f6871f81be28f02a4aaa0faaf7f4bd0ab9abfcc8
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-10-13 03:00:29 +0900

    sfsfsf

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -448,7 +448,7 @@ setup_stack (void **esp, char *file_name, char **save_ptr)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success){
-        *esp = PHYS_BASE - 4;
+        *esp = PHYS_BASE;
 
         // argument parsing
         arg_length = strlen(file_name) + strlen(*save_ptr) + 1;
