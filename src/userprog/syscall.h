276bc50db4d34f54e31387b96224e08f9ca7f7a1
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-10-15 22:03:54 +0900

    afaf

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -8,6 +8,6 @@ void syscall_init (void);
 
 void syscall_exit(struct thread *t, int status);
 
-off_t syscall_write(int fd, char* buffer, off_t size)
+off_t syscall_write(int fd, char* buffer, off_t size);
 
 #endif /* userprog/syscall.h */
