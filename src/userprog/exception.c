1873f90e09a5ff4ca717ca271bf8bc0118aff3e7
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-10-15 21:51:23 +0900

    afaf

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,6 +148,8 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+  // in case of page fault (REF 3.1.5. Accewssing User Memory)
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,8 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#include "devices/shutdown.h"
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -15,6 +17,41 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  //printf ("system call!\n");
+  //thread_exit ();
+
+  // system call handling
+  switch(*(int*)(f->esp)){
+    case SYS_HALT:	shutdown_power_off();	break;
+    case SYS_EXIT:	syscall_exit(thread_current(), *((int*)(f->esp)+1));	break;
+    case SYS_EXEC:	break;//syscall_exec();	break; 
+    case SYS_WAIT:	break;//syscall_wait();	break;
+    case SYS_CREATE:break;//syscall_create();	break;
+    case SYS_REMOVE:break;//syscall_remove();	break;
+    case SYS_OPEN:	break;//syscall_open();		break;
+    case SYS_FILESIZE:	break;//syscall_filesize();	break;
+    case SYS_READ: 		break;//syscall_read();		break;
+    case SYS_WRITE: 	syscall_write(*((int*)(f->esp)+1)
+    								  *((char**)(f->esp)+2)
+    								  *((off_t*)(f->esp)+3));	break;
+    case SYS_SEEK:		break;//syscall_seek();		break;
+    case SYS_TELL: 		break;//syscall_tell();		break;
+    case SYS_CLOSE: 	break;//syscall_close();	break;
+    default: break;
+  }
+}
+
+
+void 
+syscall_exit(struct thread *t, int status){ 
+	printf (“%s: exit(%d)\n”, t->name, status);
+	thread_exit();
+}
+
+off_t
+syscall_write(int fd, char* buffer, off_t size){
+	if(fd==1){
+		putbuf(pagedir_get_page,size);
+	}
+
 }
