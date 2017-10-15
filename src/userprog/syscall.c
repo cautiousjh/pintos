950d60fc6dfb1481b27f3b121347ba6dd4804e66
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-10-15 21:59:23 +0900

    sfsf

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,7 +5,6 @@
 #include "threads/thread.h"
 
 #include "devices/shutdown.h"
-#include "filesys/off_t.h"
 
 static void syscall_handler (struct intr_frame *);
 
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,8 +1,13 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "threads/thread.h"
+#include "filesys/off_t.h"
+
 void syscall_init (void);
 
 void syscall_exit(struct thread *t, int status);
+
+off_t syscall_write(int fd, char* buffer, off_t size)
 
 #endif /* userprog/syscall.h */
