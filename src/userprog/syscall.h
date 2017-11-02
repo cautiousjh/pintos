988e8d58f110935465e8d86a00f6277fb7844d34
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-02 21:49:33 +0900

    sfsf

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -9,7 +9,7 @@ void syscall_init (void);
 
 pid_t syscall_exec(char* cmd_str);
 void syscall_exit(int status);
-int syscall_wait(pid_t pid)
+int syscall_wait(pid_t pid);
 
 off_t syscall_write(int fd, char* buffer, off_t size);
 
