a37c5560d416c100b00ca3c88ffa12b0b3af92b2
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-03 00:59:19 +0900

    af

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -87,9 +87,8 @@ start_process (void *file_name_)
   palloc_free_page (file_name);
   if (!success) {
     curr_thread->parent->isChildLoaded = false;
+    syscall_exit(-1);
     sema_up(&curr_thread->parent->sema_load);
-    thread_current->exit_code = -1;
-    thread_exit();
   }
   else{
     curr_thread->parent->isChildLoaded = true;
