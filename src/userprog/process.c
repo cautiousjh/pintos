1a5a711128b8113b3e271868778f2cfe0544b79c
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-20 20:14:40 +0900

    sdf

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -490,17 +490,14 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
       /* Get a page of memory. */
-      struct frame* new_frame = malloc(sizeof(struct frame));
-      new_frame->upage = upage;
-      uint8_t *kpage = frame_alloc(new_frame);
+      uint8_t *kpage = palloc_get_page (PAL_USER);
       if (kpage == NULL)
         return false;
-      hash_insert(&frames,&new_frame->elem);
 
       /* Load this page. */
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
-          free_frame (new_frame);
+          palloc_free_page (kpage);
           return false; 
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
@@ -508,14 +505,15 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable)) 
         {
-          free_frame (new_frame);
+          palloc_free_page (kpage);
           return false; 
         }
-
+      
       /* Advance. */
       read_bytes -= page_read_bytes;
       zero_bytes -= page_zero_bytes;
       upage += PGSIZE;
+      
     }
   return true;
 }
