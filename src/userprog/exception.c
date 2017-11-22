64a3b15e2b55715ce1b3e06e2146a9932623b07c
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-23 07:40:22 +0900

    sgf

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -2,11 +2,13 @@
 #include "userprog/syscall.h"
 #include <inttypes.h>
 #include <stdio.h>
+#include <string.c>
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
-#include <string.c>
+#include "vm/frame.h"
+#include "vm/page.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
