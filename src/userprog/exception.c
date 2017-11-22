9c51ed0e5af806ec33dc1ac3e227d84cb8806acd
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-23 07:39:14 +0900

    sdg

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -5,6 +5,8 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include <string.c>
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
