c5f3df1063da1159b5864f1024ca5e912f2b87d4
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-24 06:08:21 +0900

    sdfdsf

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -189,8 +189,8 @@ page_fault (struct intr_frame *f)
       success = false;
     }
     // fill rest with 0
-    //if(PGSIZE - fault_page->read_bytes != 0)
-    //  memset (new_frame->kpage + fault_page->read_bytes, 0, PGSIZE - fault_page->read_bytes);
+    if(PGSIZE - fault_page->read_bytes != 0)
+      memset (new_frame->kpage + fault_page->read_bytes, 0, PGSIZE - fault_page->read_bytes);
 
     // install page into frame
     if(!install_page(fault_page->addr, new_frame->kpage, fault_page->writable))
