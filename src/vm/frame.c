6c6691f48ac1d4ea1ebbbc818476dcb03eb2e972
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-24 11:31:17 +0900

    sdf

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -9,7 +9,7 @@ void
 frames_init(void)
 {
 	lock_init(&frame_lock);
-	hash_init(&frames, frame_hash_func, frame_less_func, NULL);
+	list_init(&frames);
 }
 
 void*
@@ -22,7 +22,7 @@ frame_alloc(struct frame* new_frame)
 	lock_release(&frame_lock);
 
 	if(new_frame->kpage){
-      	hash_insert(&frames,&new_frame->elem);
+      	list_push_back(&frames,&new_frame->elem);
 		return new_frame->kpage;
 	}
 	else{
@@ -34,7 +34,7 @@ frame_alloc(struct frame* new_frame)
     	pagedir_set_accessed(new_frame->t->pagedir, new_frame->kpage, true);
 		lock_release(&frame_lock);
 
-      	hash_insert(&frames,&new_frame->elem);
+      	list_push_back(&frames,&new_frame->elem);
 		return new_frame->kpage;
 	}
 }
@@ -52,64 +52,37 @@ void*
 frame_evict(struct frame* f) 
 {
 	struct frame* temp_frame;
-	struct hash_iterator iter;
+	struct list_elem* iter;
 
-	hash_first(&iter, &frames);
-	while(hash_next(&iter)){
-		temp_frame = hash_entry(hash_cur(&iter), struct frame, elem);
+
+  	for(iter = list_begin(&frames);
+      	iter != list_end(&frames);
+      	iter = iter->next){
+  		temp_frame = list_entry(iter, struct frame, elem);
 		if(temp_frame->kpage){
 			if(pagedir_is_accessed(temp_frame->t->pagedir, temp_frame->related_page->addr))
 				pagedir_set_accessed(temp_frame->t->pagedir, temp_frame->related_page->addr, false);
 			else
-				frame_swap(temp_frame);
-		}
-	}
+				frame_swap(temp_frame);  		
+  	}
 
-	hash_first(&iter, &frames);
-	while(hash_next(&iter)){
+  	for(iter = list_begin(&frames);
+      	iter != list_end(&frames);
+      	iter = iter->next){
 		temp_frame = hash_entry(hash_cur(&iter), struct frame, elem);
 		if(temp_frame->kpage){
 			if(!pagedir_is_dirty(temp_frame->t->pagedir, temp_frame->related_page->addr))
-				frame_swap(temp_frame);
-		}
-	}
+				frame_swap(temp_frame); 		
+  	}
 
-	hash_first(&iter, &frames);
-	while(hash_next(&iter)){
+  	for(iter = list_begin(&frames);
+      	iter != list_end(&frames);
+      	iter = iter->next){
 		temp_frame = hash_entry(hash_cur(&iter), struct frame, elem);
 		if(temp_frame->kpage)
-			frame_swap(temp_frame);
-	}
+			frame_swap(temp_frame); 		
+  	}
+
 	free(temp_frame);
 	return NULL;
-}
-
-void*
-frame_swap(struct frame* victim)
-{
-	// memory to swap_disk
-	swap_out(victim->related_page);
-	victim->related_page->frame_entry = NULL;
-	victim->related_page->status = IN_SWAP_TABLE;
-	victim->related_page->kpage = victim->kpage;
-	palloc_free_page(victim->kpage);
-	pagedir_clear_page(victim->t->pagedir, victim->related_page->addr);
-	return NULL;
-}
-
-unsigned
-frame_hash_func(const struct hash_elem *e,void *aux UNUSED)
-{
-	const struct frame* f = hash_entry(e, struct frame, elem);
-	return hash_bytes(&f->upage, sizeof(f->upage));
-}
-
-bool 
-frame_less_func(const struct hash_elem *_a,
-                const struct hash_elem *_b,
-                void *aux UNUSED)
-{
-	struct frame* a = hash_entry(_a, struct frame, elem);
-	struct frame* b = hash_entry(_b, struct frame, elem);
-	return a->upage != b->upage? a->upage < b->upage : a->t->tid < b->t->tid;
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,7 +1,7 @@
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
 
-#include <hash.h>
+#include <list.h>
 #include "threads/thread.h"
 #include "threads/synch.h"
 #include "threads/malloc.h"
@@ -9,7 +9,7 @@
 #include "filesys/off_t.h"
 #include "vm/swap.h"
 
-struct hash frames;
+struct list frames;
 
 
 struct frame{
@@ -17,7 +17,7 @@ struct frame{
 	void* upage;
 	struct thread* t;
 	struct page* related_page;
-	struct hash_elem elem;
+	struct list_elem elem;
 };
 
 void frames_init(void);
@@ -25,11 +25,5 @@ void* frame_alloc(struct frame* f);
 void* frame_evict(struct frame* f);
 void* frame_swap(struct frame* victim);
 void free_frame (struct frame* f);
-
-unsigned frame_hash_func(const struct hash_elem *e,void *aux);
-bool frame_less_func(const struct hash_elem *a,
-                     const struct hash_elem *b,
-                     void *aux);
-
 
 #endif
