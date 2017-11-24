67030a9764248cad154b74b4870373fc5df2df02
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-24 11:51:01 +0900

    sdf

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -62,8 +62,12 @@ frame_evict(struct frame* f)
 		if(temp_frame->kpage)
 			if(pagedir_is_accessed(temp_frame->t->pagedir, temp_frame->related_page->addr))
 				pagedir_set_accessed(temp_frame->t->pagedir, temp_frame->related_page->addr, false);
-			else
-				frame_swap(temp_frame);  		
+			else{
+  				list_remove(&temp_frame->elem);
+				free(temp_frame);
+				frame_swap(temp_frame);  
+				return;		
+			}
   	}
 
   	for(iter = list_begin(&frames);
@@ -71,20 +75,25 @@ frame_evict(struct frame* f)
       	iter = iter->next){
 		temp_frame = list_entry(iter, struct frame, elem);
 		if(temp_frame->kpage)
-			if(!pagedir_is_dirty(temp_frame->t->pagedir, temp_frame->related_page->addr))
-				frame_swap(temp_frame); 		
+			if(!pagedir_is_dirty(temp_frame->t->pagedir, temp_frame->related_page->addr)){
+  				list_remove(&temp_frame->elem);
+				free(temp_frame);
+				frame_swap(temp_frame);  
+				return;		
+			}		
   	}
 
   	for(iter = list_begin(&frames);
       	iter != list_end(&frames);
       	iter = iter->next){
 		temp_frame = list_entry(iter, struct frame, elem);
-		if(temp_frame->kpage)
-			frame_swap(temp_frame); 		
+		if(temp_frame->kpage){
+  			list_remove(&temp_frame->elem);
+			free(temp_frame);
+			frame_swap(temp_frame);  
+			return;		
+		}		
   	}
-  	list_remove(&temp_frame->elem);
-	free(temp_frame);
-	return NULL;
 }
 
 
