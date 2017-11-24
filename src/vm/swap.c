653463c9d4fb9755b314515d469d03d9c561d387
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-24 09:55:52 +0900

    sdf

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -13,8 +13,8 @@ struct block *swap_table;
 void 
 swap_init(void)
 {
+	blocks = bitmap_create(1<<9);
 	swap_table = block_get_role(BLOCK_SWAP);
-	blocks = bitmap_create(block_size(swap_table)/PGSIZE*BLOCK_SECTOR_SIZE);
 	lock_init(&block_lock);
 }
 
@@ -22,12 +22,12 @@ swap_init(void)
 void
 swap_in(struct page* p)
 {
-	int sector, i;
+	int i;
 
 	lock_acquire(&block_lock);
 	p->sector = bitmap_scan_and_flip(blocks, 0, 1, false);
 	for(i=0;i<PGSIZE/BLOCK_SECTOR_SIZE;i++){
-		block_read(swap_table, sector*PGSIZE/BLOCK_SECTOR_SIZE+i, 
+		block_read(swap_table, p->sector*PGSIZE/BLOCK_SECTOR_SIZE+i, 
 				   p->frame_entry->kpage + i*BLOCK_SECTOR_SIZE);
 	}
 	lock_release(&block_lock);
@@ -38,13 +38,13 @@ swap_in(struct page* p)
 void 
 swap_out(struct page* p)
 {	
-	int sector = p->sector, i;
+	int i;
 
 	lock_acquire(&block_lock);
 	for(i=0;i<PGSIZE/BLOCK_SECTOR_SIZE;i++)
-		block_write(swap_table, sector*PGSIZE/BLOCK_SECTOR_SIZE+i, 
+		block_write(swap_table, p->sector*PGSIZE/BLOCK_SECTOR_SIZE+i, 
 				   p->frame_entry->kpage + i*BLOCK_SECTOR_SIZE);
-	bitmap_reset(blocks,sector);
+	bitmap_reset(blocks,p->sector);
 	lock_release(&block_lock);
 }
 
