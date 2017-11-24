4509e752a2b355432c1bbd5a54bc26ca158c6452
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-11-24 12:15:01 +0900

    sdf

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -26,7 +26,7 @@ swap_in(struct page* p)
 	lock_acquire(&block_lock);
 	for(i=0;i<PGSIZE/BLOCK_SECTOR_SIZE;i++){
 		block_read(swap_table, p->sector*PGSIZE/BLOCK_SECTOR_SIZE+i, 
-				   p->frame_entry->kpage + i*BLOCK_SECTOR_SIZE);
+				   p->kpage + i*BLOCK_SECTOR_SIZE);
 	}
 	bitmap_reset(blocks,p->sector);
 	lock_release(&block_lock);
