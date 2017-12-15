d3b9cd034b1fd379558517c6e1af8e8072a97102
cautiousjh <31879262+cautiousjh@users.noreply.github.com>
2017-12-15 16:51:38 +0900

    zxvzvxxzv

--- src/filesys/file.c
+++ src/filesys/file.c
@@ -96,7 +96,7 @@ off_t
 file_write (struct file *file, const void *buffer, off_t size) 
 {
   // TODOTODOTODOTODOTODO
-  if(inode_get_parent(file->inode) == -1) // if not directory
+  if(inode_get_parent(file->inode) != -1) // if directory
       return -1;
   off_t bytes_written = inode_write_at (file->inode, buffer, size, file->pos);
   file->pos += bytes_written;
@@ -114,7 +114,7 @@ off_t
 file_write_at (struct file *file, const void *buffer, off_t size,
                off_t file_ofs) 
 {
-  if(inode_get_parent(file->inode) == -1) // if not directory
+  if(inode_get_parent(file->inode) != -1) // if directory
     return -1;
   return inode_write_at (file->inode, buffer, size, file_ofs);
 }
